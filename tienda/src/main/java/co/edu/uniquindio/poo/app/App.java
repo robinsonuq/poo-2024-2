a3b17e67ed287c705cca23efca907a17e8c1de08
racionaldev <86496632+racionaldev@users.noreply.github.com>
2024-08-28 07:03:38 -0500

    update

---
+++ tienda/src/main/java/co/edu/uniquindio/poo/app/App.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ tienda/src/main/java/co/edu/uniquindio/poo/model/Tienda.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.model;
+
+public class Tienda {
+
+}
